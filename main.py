9e75a5b0f05d3ded8d145fc571b201281306b6c5
Peter Nielsen <dirigo112@gmail.com>
2020-04-21 15:09:15 -0700

    Easier reading, abilities
    Added trench.png, a background for later levels. main.py has been updated to give things correct namings, in addition to a variable conflict with the "quaduse" variable [new with this commit] (mentions and loops have been mixed up and must be corrected). Powerups have been started on.

--- main.py
+++ main.py
@@ -11,12 +11,13 @@ from pygame.locals import *
 TITLE = 'Space Invaders: "A New Hope" Edition'
 
 
-WIDTH = 250
-HEIGHT = 500
+WIDTH = 384
+HEIGHT = 683
 SPEED = 5
-ufo_speed = 9
-ufostart = (random.randint(1,250))
+tie_speed = 9
+tiestart = (random.randint(1,250))
 
+quaduse = 0
 
 #music
 pygame.mixer.music.load('theme.mp3')
@@ -28,147 +29,185 @@ pygame.mixer.music.play(loops=-1)
 class Game():
 	def __init__(self):
 		self.score = 0
+		self.level = 1
 
 game = Game()
 
 
 #actors
 #bad guys
-ufo = Actor('tiefighter', (ufostart, 0))
-ufo.y = ufo.height/2
+tie = Actor('tiefighter', (tiestart, 0))
+tie.y = tie.height/2
 
-ufo2 = Actor('tiefighter', (ufostart, 0))
-ufo2.y = ufo2.height/2
+tie2 = Actor('tiefighter', (tiestart, 0))
+tie2.y = tie2.height/2
 
-bigufo = Actor('deethstarr', (ufostart, 0))
+tie3 = Actor('tiefighter', (tiestart, 0))
+tie3.y = tie3.height/2
+
+bigtie = Actor('deethstarr', (tiestart, 0))
 
 #blasts
-pizza = Actor('laser', (-WIDTH, -HEIGHT))
-pizza.active = False
+laser = Actor('laser', (-WIDTH, -HEIGHT))
+laser.active = False
 
 quadcannonblast = Actor('morelaser', (-WIDTH, -HEIGHT))
 quadcannonblast.active = False
 
 #x-wing
-rocketship = Actor('falcon', (WIDTH/2, HEIGHT))
-rocketship.y = HEIGHT - rocketship.height/2
+ship = Actor('falcon', (WIDTH/2, HEIGHT))
+ship.y = HEIGHT - ship.height/2
 
 
 #define functions
 
 
-ufo_speed == game.score + 9
+tie_speed == game.score + 9
 
 
 #laser fires
-def pizza_motion():
-	if pizza.active == True:
-		pizza.y -= SPEED
+def laser_motion():
+	if laser.active == True:
+		laser.y -= SPEED
 def fire():
-	pizza.x = rocketship.x
-	pizza.y = rocketship.y - rocketship.height/2 - pizza.height/2
+	laser.x = ship.x
+	laser.y = ship.y - ship.height/2 - laser.height/2
 
 def quad_motion():
 	if quadcannonblast.active == True:
 		quadcannonblast.y -= SPEED
+
 def fire_quad():
-	quadcannonblast.x = rocketship.x
-	quadcannonblast.y = rocketship.y - rocketship.height/2 - quadcannonblast.height/2
+	quadcannonblast.x = ship.x
+	quadcannonblast.y = ship.y - ship.height/2 - quadcannonblast.height/2
 
 #x-wing movmement
 def get_keyboard(SPEED):
 	if keyboard.left:
-		rocketship.x -= SPEED
+		ship.x -= SPEED
 	elif keyboard.right:
-		rocketship.x += SPEED
+		ship.x += SPEED
 	elif keyboard.space:
-		pizza.active = True
+		laser.active = True
 		fire()
 	elif keyboard.down:
 		quadcannonblast.active = True
 		fire_quad()
 
-def reset_ufo():
-	ufo.y = 0
-	ufo.x = random.randint(0 + ufo.width, WIDTH - ufo.width)
+def reset_tie():
+	tie.y = 0
+	tie.x = random.randint(0 + tie.width, WIDTH - tie.width)
 
-def reset_ufo2():
-	ufo2.y = 0
-	ufo2.x = random.randint(0 + ufo.width, WIDTH - ufo.width)
+def reset_tie2():
+	tie2.y = 0
+	tie2.x = random.randint(0 + tie.width, WIDTH - tie.width)
 
-def rocketship_kill():
-	rocketship.x = WIDTH/2
+def reset_tie3():
+	tie3.y = 0
+	tie3.x = random.randint(0 + tie.width, WIDTH - tie.width)
 
-def ufo_get_past():
-	if ufo.y == HEIGHT:
-		reset_ufo()
+def ship_kill():
+	ship.x = WIDTH/2
+
+def tie_get_past():
+	if tie.y == HEIGHT:
+		reset_tie()
 		game.score -= 1
-	elif ufo2.y == HEIGHT:
-		reset_ufo2()
+	if tie2.y == HEIGHT:
+		reset_tie2()
+		game.score -= 1
+	if tie3.y == HEIGHT:
+		reset_tie3()
 		game.score -= 1
 
-def reset_pizza():
-	pizza.pos = (-WIDTH, -HEIGHT)
-	pizza.active = False
+def reset_laser():
+	laser.pos = (-WIDTH, -HEIGHT)
+	laser.active = False
 
 def reset_quad():
 	quadcannonblast.pos = (-WIDTH, -HEIGHT)
 	quadcannonblast.active = False
 
 def out_screen():
-	if rocketship.x > WIDTH:
-		rocketship_kill()
+	if ship.x > WIDTH:
+		ship_kill()
 		game.score -= 1
 
 def test_hit():
-	if ufo.colliderect(pizza):
-		reset_ufo()
-		reset_pizza()
+	if tie.colliderect(laser):
+		reset_tie()
+		reset_laser()
 		game.score += 1
-	elif ufo2.colliderect(pizza):
-		reset_ufo2()
-		reset_pizza()
+	elif tie2.colliderect(laser):
+		reset_tie2()
+		reset_laser()
 		game.score += 1
-	elif ufo.colliderect(quadcannonblast):
-		reset_ufo()
-		reset_quad()
+	elif tie3.colliderect(laser):
+		reset_tie3()
+		reset_laser()
 		game.score += 1
-	elif ufo2.colliderect(quadcannonblast):
-		reset_ufo2()
-		reset_quad()
+	elif tie.colliderect(quadcannonblast):
+		reset_tie()
+		quaduse += 1
+		if quaduse == 2:
+			reset_quad()
+			quaduse -= 2
+		game.score += 1
+	elif tie2.colliderect(quadcannonblast):
+		reset_tie2()
+		quaduse += 1
+		if quaduse == 2:
+			reset_quad()
+			quaduse -= 2
+		game.score += 1
+	elif tie3.colliderect(quadcannonblast):
+		reset_tie3()
+		quaduse += 1
+		if quaduse == 2:
+			reset_quad()
+			quaduse -= 2
 		game.score += 1
 
-def ufo_motion():
-	ufo.y += ufo_speed/3
-	if ufo.y > HEIGHT:
-		reset_ufo()
-	ufo2.y += ufo_speed/3
-	if ufo2.y > HEIGHT:
-		reset_ufo2()
+def tie_motion():
+	tie.y += tie_speed/3
+	if tie.y > HEIGHT:
+		reset_tie()
+	tie2.y += tie_speed/3
+	if tie2.y > HEIGHT:
+		reset_tie2()
+	tie3.y += tie_speed/3
+	if tie3.y > HEIGHT:
+		reset_tie3()
 
 def deathstarmotion():
-	bigufo.y += SPEED
+	bigtie.y += SPEED
 
 
 #execute main functions
 def update():
 	get_keyboard(SPEED)
-	ufo_motion()
-	pizza_motion()
+	tie_motion()
+	laser_motion()
 	test_hit()
 	out_screen()
-	ufo_get_past()
+	tie_get_past()
 	deathstarmotion()
 	quad_motion()
 
 def draw():
 	screen.clear()
-	rocketship.draw()
-	if game.score < 20:
-		ufo.draw()
-		ufo2.draw()
-	pizza.draw()
+	ship.draw()
+	tie.draw()
+	tie2.draw()
+	tie3.draw()
+#	if game.score < 20:
+#		tie.draw()
+#		tie2.draw()
+	laser.draw()
+	laser.draw()
+	laser.draw()
+	quadcannonblast.draw()
 	screen.draw.text(str(game.score), (WIDTH/20, HEIGHT/20))
-	if game.score >= 20:
-		bigufo.draw()
+#	if game.score >= 20:
+#		bigtie.draw()
 pgzrun.go()
