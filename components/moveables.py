ecf354cdd8af331e04046039ae3a06370499a94d
Peter Nielsen <dirigo112@gmail.com>
2020-04-23 13:59:44 -0700

    Added moduling out options, easier to read.
    Organizing the code, and also creating files.

---
+++ components/moveables.py
@@ -0,0 +1,8 @@
+import pgzrun
+import random
+import pygame
+import os
+import sys
+import pygame.locals as pl
+from pygame.locals import *
+from main import laser
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#import images
+#import commands
 import pgzrun
 import random
 import pygame
@@ -8,16 +8,8 @@ import pygame.locals as pl
 from pygame.locals import *
 
 
-#falcon
-#awing
-#xwing
-#razorcrest
-
-
 #constants
 TITLE = 'Space Invaders: "A New Hope" Edition'
-
-
 WIDTH = 384
 HEIGHT = 683
 SPEED = 5
@@ -25,11 +17,13 @@ tie_speed = 9
 tiestart = (random.randint(1,250))
 BACKGROUND_IMAGE = 'background'
 
+
 #music
 pygame.mixer.music.load('theme.mp3')
-#pygame.mixer.music.play(loops=-1)
+pygame.mixer.music.play(loops=-1)
 
-#attributes for the game
+
+#game attributes [score, level, ship, etc]
 class Game():
     def __init__(self):
         self.score = 0
@@ -44,68 +38,60 @@ class Game():
             self.ship = sys.argv[1]
         else:
             self.ship = 'xwing'
-#specify ship can be 'falcon', 'awing', 'razorcrest', or 'xwing'
 game = Game()
 
 
-
 #actors
-#bad guys
+#tie fighters
 tie = Actor('tiefighter', (tiestart, 0))
 tie.y = tie.height/2
-
 tie2 = Actor('tiefighter', (tiestart, 0))
 tie2.y = tie2.height/2
-
 tie3 = Actor('tiefighter', (tiestart, 0))
 tie3.y = tie3.height/2
-
-bigtie = Actor('deethstarr', (tiestart, 0))
-
-#blasts
+#standard laser, universal
 laser = Actor('laser', (-WIDTH, -HEIGHT))
 laser.active = False
-
+#quad cannon blast, for the Millenium Falcon's ability
 quadcannonblast = Actor('morelaser', (-WIDTH, -HEIGHT))
 quadcannonblast.active = False
-
+#ion blast, for the a=wing ability
 ion = Actor('ion', (-WIDTH, -HEIGHT))
 ion.active = False
-
+#proton torpedo, for the x-wing ability
 protontorp = Actor('torp', (-WIDTH, -HEIGHT))
 protontorp.active = False
-
 #x-wing
 ship = Actor(game.ship, (WIDTH/2, HEIGHT))
 ship.y = HEIGHT - ship.height/2
 
 
-#define functions
-
-
+#set tie fighter speed
 tie_speed == (game.score/300) + 9
 
 
-#laser fires
+#laser functions
 def laser_motion():
     if laser.active == True:
         laser.y -= SPEED
 def fire():
     laser.x = ship.x
     laser.y = ship.y - ship.height/2 - laser.height/2
-
+#quad cannon functions
 def quad_motion():
     if quadcannonblast.active == True:
         quadcannonblast.y -= SPEED
 def fire_quad():
     quadcannonblast.x = ship.x
     quadcannonblast.y = ship.y - ship.height/2 - quadcannonblast.height/2
+#ion blast functions
 def ion_motion():
     if ion.active == True:
         ion.y -= SPEED
 def fire_ion():
     ion.x = ship.x
     ion.y = ship.y - ship.height/2 - ion.height/2
+#proton torpedo functions
 def protontorp_motion():
     if protontorp.active == True:
         protontorp.y -= SPEED
@@ -113,7 +99,8 @@ def fire_protontorp():
     protontorp.x = ship.x
     protontorp.y = ship.y - ship.height/2 - protontorp.height/2
 
-#x-wing movmement
+
+#movement and keyboard input
 def get_keyboard(SPEED):
     if keyboard.left:
         ship.x -= SPEED
@@ -134,44 +121,43 @@ def get_keyboard(SPEED):
         if keyboard.down:
             protontorp.active = True
             fire_protontorp()
+
+
+#resets
+#tie fighters
 def reset_tie():
     tie.y = 0
     tie.x = random.randint(0 + tie.width, WIDTH - tie.width)
-
 def reset_tie2():
     tie2.y = 0
     tie2.x = random.randint(0 + tie.width, WIDTH - tie.width)
-
 def reset_tie3():
     tie3.y = 0
     tie3.x = random.randint(0 + tie.width, WIDTH - tie.width)
-
+#player's ship
 def ship_kill():
     ship.x = WIDTH/2
     game.score -= 100
-
-
-def reset_laser():
-    laser.pos = (-WIDTH, -HEIGHT)
-    laser.active = False
-
-def reset_quad():
-    quadcannonblast.pos = (-WIDTH, -HEIGHT)
-    quadcannonblast.active = False
-
-def reset_ion():
-    ion.pos = (-WIDTH, -HEIGHT)
-    ion.active = False
-
-def reset_torp():
-    protontorp.pos = (-WIDTH, -HEIGHT)
-    protontorp.active = False
-
 def out_screen():
     if ship.x > WIDTH:
         ship_kill()
         game.score -= 100
+#weaponry
+def reset_laser():
+    laser.pos = (-WIDTH, -HEIGHT)
+    laser.active = False
+def reset_quad():
+    quadcannonblast.pos = (-WIDTH, -HEIGHT)
+    quadcannonblast.active = False
+def reset_ion():
+    ion.pos = (-WIDTH, -HEIGHT)
+    ion.active = False
+def reset_torp():
+    protontorp.pos = (-WIDTH, -HEIGHT)
+    protontorp.active = False
 
+
+#check for hits
 def test_hit():
     if tie.colliderect(laser):
         reset_tie()
@@ -192,7 +178,6 @@ def test_hit():
         if game.quaduse == 2:
             reset_quad()
             game.quaduse -= 2
-
     elif tie2.colliderect(quadcannonblast):
         reset_tie2()
         game.quaduse += 1
@@ -200,7 +185,6 @@ def test_hit():
         if game.quaduse == 2:
             reset_quad()
             game.quaduse -= 2
-
     elif tie3.colliderect(quadcannonblast):
         reset_tie3()
         game.quaduse += 1
@@ -224,8 +208,8 @@ def test_hit():
         game.score += 100
 
 
-
-
+#motion
+#tie fighters
 def tie_motion():
     tie.y += tie_speed/3
     if tie.y > HEIGHT:
@@ -240,13 +224,9 @@ def tie_motion():
         reset_tie3()
         game.score -= 200
 
-def deathstarmotion():
-    bigtie.y += SPEED
-    if bigtie.y > HEIGHT:
-        pass
-
 
 #execute main functions
+#update
 def update():
     get_keyboard(SPEED)
     tie_motion()
@@ -257,7 +237,7 @@ def update():
     quad_motion()
     protontorp_motion()
     ion_motion()
-
+#draw
 def draw():
     screen.clear()
     screen.blit(BACKGROUND_IMAGE, (0,0))
@@ -265,14 +245,12 @@ def draw():
     tie.draw()
     tie2.draw()
     tie3.draw()
-#	if game.score < 20:
-#		tie.draw()
-#		tie2.draw()
     laser.draw()
     quadcannonblast.draw()
     ion.draw()
     protontorp.draw()
     screen.draw.text(str(game.score), (WIDTH/20, HEIGHT/20))
-#	if game.score >= 20:
-#		bigtie.draw()
+
+
+#wrap up
 pgzrun.go()
