2faa424eb25c395b146f3b4e6e14b828f45c9afd
Peter Nielsen <dirigo112@gmail.com>
2021-08-11 13:58:38 -0700

    Added extra scoring and stat tracking.

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ import time
 
 
 #constants
-WIDTH = 492
-HEIGHT = 294
+WIDTH = 500
+HEIGHT = 600
 SPEED = 5
 tie_speed = 9
 tiestart = (random.randint(1,250))
@@ -26,17 +26,23 @@ else:
 #shipchoice = sys.argv[1]
 
 #music
-pygame.mixer.music.load('theme.mp3')
-pygame.mixer.music.play(loops=-1)
+# pygame.mixer.music.load('theme.mp3')
+# pygame.mixer.music.play(loops=-1)
 
 t1 = time.time()
 print(t1)
 #game attributes [score, level, ship, etc]
 class Game():
     def __init__(self):
+        self.level1quota = 150
         self.score = 0
         self.level = 1
         self.quaduse = 0
+        self.tiesLet = 0
+        self.tiesStopped = 0
+        self.hitsHit = 0
+        self.deaths = 0
+        self.end = False
         #self.view = 'level-1'     #to test game mechanics - must be set to 'splash' in order for full functionality to be used
         self.view = 'splash'
         if len(sys.argv) >= 2:
@@ -50,6 +56,12 @@ class Game():
             self.ship = 'xwing'
 game = Game()
 
+
+def game_end():
+    time.sleep(1)
+    print(f'\n\nLevel 1 Complete!')
+    print(f'\n\n\n\nFinal Stats:\n\nScore: {game.score}\nTies Hit: {game.hitsHit}\nDeaths: {game.deaths}\nTies Let Through: {game.tiesLet}')
+    quit()
 
 #actors
 #tie fighters
@@ -154,6 +166,8 @@ def reset_tie3():
 def ship_kill():
     ship.x = WIDTH/2
     game.score -= 100
+    game.deaths += 1
+    print(f'Deaths: {game.deaths}')
 def out_screen():
     if ship.x > WIDTH:
         ship_kill()
@@ -176,53 +190,99 @@ def reset_torp():
 #check for hits
 def test_hit():
     if tie.colliderect(laser):
+        addScore = round(150 + tie.x / 5)
         reset_tie()
         reset_laser()
-        game.score += 100
+        game.score += addScore
+        print(f'Tie 3 Killed with Laser. Added {addScore} points.')
+        game.hitsHit += 1
     elif tie2.colliderect(laser):
+        add2Score = round(150 + tie3.x / 5)
         reset_tie2()
         reset_laser()
-        game.score += 100
+        game.score += add2Score
+        print(f'Tie 2 Killed with Laser. Added {add2Score} points.')
+        game.hitsHit += 1
     elif tie3.colliderect(laser):
+        add3Score = round(150 + tie3.x / 5)
         reset_tie3()
         reset_laser()
-        game.score += 100
+        game.score += add3Score
+        print(f'Tie 3 Killed with Laser. Added {add3Score} points.')
+        game.hitsHit += 1
     elif tie.colliderect(quadcannonblast):
+        addQuadScore = round(100 + tie.x / 5)
         reset_tie()
         game.quaduse += 1
-        game.score += 250
+        game.score += addQuadScore
+        print(f'Tie 1 Killed with Quad Blast. Added {addQuadScore} points.')
         if game.quaduse == 2:
             reset_quad()
             game.quaduse -= 2
+        game.hitsHit += 1
     elif tie2.colliderect(quadcannonblast):
+        addQuad2Score = round(100 + tie2.x / 5)
         reset_tie2()
         game.quaduse += 1
-        game.score += 250
+        game.score += 100
+        print(f'Tie 1 Killed with Quad Blast. Added {addQuad2Score} points.')
         if game.quaduse == 2:
             reset_quad()
             game.quaduse -= 2
+        game.hitsHit += 1
     elif tie3.colliderect(quadcannonblast):
+        addQuad3Score = round(100 + tie3.x / 5)
         reset_tie3()
         game.quaduse += 1
-        game.score += 250
+        game.score += 100
+        print(f'Tie 1 Killed with Quad Blast. Added {addQuad3Score} points.')
         if game.quaduse == 2:
             reset_quad()
             game.quaduse -= 2
+        game.hitsHit += 1
     elif tie.colliderect(ion):
+        addIonScore = round(100 + ((tie3.x + tie2.x + tie.x) / 5))
         reset_tie()
         reset_tie2()
         reset_tie3()
-        game.score += 1000
+        game.score += addIonScore
+        print(f'Tie 1 hit with Ion Blast. All Ties killed. Added {addIonScore} points.')
+        game.hitsHit += 3
+    elif tie2.colliderect(ion):
+        addIon2Score = round(100 + ((tie3.x + tie2.x + tie.x) / 5))
+        reset_tie()
+        reset_tie2()
+        reset_tie3()
+        game.score += addIon2Score
+        print(f'Tie 2 hit with Ion Blast. All Ties killed. Added {addIon2Score} points.')
+        game.hitsHit += 3
+    elif tie3.colliderect(ion):
+        addIon3Score = round(100 + ((tie3.x + tie2.x + tie.x) / 5))
+        reset_tie()
+        reset_tie2()
+        reset_tie3()
+        game.score += addIon3Score
+        print(f'Tie 3 hit with Ion Blast. All Ties killed. Added {addIon3Score} points.')
+        game.hitsHit += 3
     elif tie.colliderect(protontorp):
         reset_tie()
         game.score += 100
+        game.hitsHit += 1
     elif tie2.colliderect(protontorp):
         reset_tie2()
         game.score += 100
+        game.hitsHit += 1
     elif tie3.colliderect(protontorp):
         reset_tie3()
         game.score += 100
+        game.hitsHit += 1
 
+
+def stopScoreFromZero():
+    if game.score <= 0:
+        game.score = 0
+    else:
+        pass
 
 #motion
 #tie fighters
@@ -231,14 +291,17 @@ def tie_motion():
     if tie.y > HEIGHT:
         reset_tie()
         game.score -= 200
+        game.tiesLet += 1
     tie2.y += tie_speed/3
     if tie2.y > HEIGHT:
         reset_tie2()
         game.score -= 200
+        game.tiesLet += 1
     tie3.y += tie_speed/3
     if tie3.y > HEIGHT:
         reset_tie3()
         game.score -= 200
+        game.tiesLet += 1
 
 
 #execute main functions
@@ -252,6 +315,17 @@ def update():
     quad_motion()
     protontorp_motion()
     ion_motion()
+    stopScoreFromZero()
+    if game.level == 1:
+        if game.level1quota <= game.hitsHit:
+            game.end = True
+    else:
+        pass
+    if game.end == True:
+        game_end()
+    else:
+        pass
+        
 #draw
 def draw():
     if game.view == 'level-1':
@@ -265,14 +339,19 @@ def draw():
         quadcannonblast.draw()
         ion.draw()
         protontorp.draw()
-        screen.draw.text(str(game.score), (WIDTH/20, HEIGHT/20))
+        screen.draw.text(str(f'Score: {game.score}'), (WIDTH/20, HEIGHT/20))
+        screen.draw.text(str(f'Ties Killed: {game.hitsHit}'), (WIDTH/20, HEIGHT/30 - 5))
+        screen.draw.text(str(f'Deaths: {game.deaths}'), (WIDTH/20, HEIGHT/10 - 15))
+        screen.draw.text(str(f'Ties Escaped: {game.tiesLet}'), (WIDTH/20, HEIGHT/10))
     elif game.view == 'splash':
         screen.clear()
         screen.blit('logo', (0,0))
-        screen.draw.text(str('<Press the DOWN key to begin>'), (WIDTH - 300, HEIGHT - 15))
+        screen.draw.text(str('<Press the DOWN key to begin>'), (WIDTH / 2, HEIGHT - 15))
     else:
         pass
 
 
 #wrap up
 pgzrun.go()
+
+game_end()
